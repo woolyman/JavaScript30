2c19d9f681efcf09e2c8a22d91741815895d5c65
Sean Woleslagle <jacx92@gmail.com>
2020-01-28 14:57:10 -0500

    finished the first project

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,8 +1,17 @@
-window.addEventListener('keydown', function(e) {
+function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if (!audio) return; // If there's no audio, stop the function from running all together
     audio.currentTime = 0; // Rewind audio to the start
     audio.play();
-    console.log(key);
-});
+    key.classList.add('playing');
+}
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return; // Skip it if it is not a transform
+    this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
